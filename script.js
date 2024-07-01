6b8d5c197f77ed04771f3081f2e3bc910ff6f087
Oluwaseyi Tommy <130176232+seshconcept@users.noreply.github.com>
2024-07-01 14:47:53 +0100

    update

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const now = new Date();
-const currentTimeUTC = now.toUTCString();
+function dateDisplay() {
+  const dayofWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const now = new Date();
 
-const utcTimeElement = document.querySelector('[data-testid="utc-time"]');
-utcTimeElement.textContent = currentTimeUTC;
+  const timeUTC = document.querySelector('[data-testid="currentTimeUTC"]');
+  const utcday = document.querySelector('[data-testid="currentDay"]');
+  timeUTC.innerHTML = `Current Time in UTC : ${now.toUTCString()}`;
+  utcday.innerHTML = `Current Day of the week : ${dayofWeek[now.getDay()]}`;
 
-
-function updateUTCDay() {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const now = new Date();
-    const dayOfWeekUTC = daysOfWeek[now.getUTCDay()];
-
-    const utcDayElement = document.document.querySelector('[data-testid="utc-day"]');
-    utcDayElement.textContent = `Current day in UTC: ${dayOfWeekUTC}`;
+  setInterval(dateDisplay, 1000);
 }
 
+dateDisplay();
